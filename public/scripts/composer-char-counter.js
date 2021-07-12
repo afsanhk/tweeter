d8dd0de5043fe6fa34559feeb9e36bb1954c61a3
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-12 23:33:54 +0000

    Delayed git commit. Added new CSS sheets: 1)nav.css 2)header.css 3)new-tweet.css to implement the layout of the Tweeter SPA. nav.css encompasses the styling rules for the Tweeter nav-bar. header.css implements rules for the avatar, username and overall header. new-tweet.css governs style rules for new tweets made within the <main> HTML section of index.html. Furthermore, added first JS script utilzing JS in composer-char-counter.js which a) updates the character counter based on characters entered using an input event handler, b) changes the color of the character counter if the max character limit has been passed. Implemented this within the bottom of the HTML within <script src=> </script> tags. Also modified layout.css slightly to ensure that the <main> HTML (container class) was centered and had design according to the given specifications.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  
+  console.log('Composer Character Counter Script Is Ready');
+  
+  $("#tweet-text").on('input', function() { // Use input instead of keyup/down/press because it counts copy/paste.
+    // Note: $(this) selects #tweet-area as the 'target' for our methods
+    let charCount = $(this).val().length; // $(this).val().length --> .val() returns what is inside the text area and .length returns the length i.e characters entered.
+
+    // Set charCounter to the .counter class element
+    const maxLength = 140; // The max length is 140. Don't set this equal to countElement.val() as the value of $('.counter') will decrease as you input more chars.
+    $(this).siblings('div').children('.counter').text(maxLength - charCount);
+
+    // Set font color to red if more than 140 chars
+    if (charCount > 140) {
+      $(this).siblings('div').children('.counter').addClass('error-tweet-text'); // This style is defined in new-tweet.css and only activates when >140 chars.
+      // $(this).siblings('div').children('.counter').css("color", "red"); // This what you would use for a direct CSS change
+    } else {
+      $(this).siblings('div').children('.counter').removeClass('error-tweet-text'); // Class is removed and styling is back to default when <140 chars.
+      // $(this).siblings('div').children('.counter').css("color", "#545149"); // This is what you would use for a direct CSS change
+    }
+
+  });
+  
+});
+
+
