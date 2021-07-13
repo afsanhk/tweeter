cdea40034aa5d46feffbecabedcccea61a625580
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-13 21:52:49 +0000

    Fixed space issues, added comments for easier readability and ran eslint . --fix on the directory.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,52 +3,53 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function () {
+$(document).ready(function() {
 
-  // Creates a single tweet article
-  const createTweetElement = function (data) {
+  // 1. Function creates a single tweet article
+  const createTweetElement = function(data) {
     const $output = $('<article class="tweet">'); // Output is a tweet class article
     
     // Article Header Tags
     const $articleHeader = $('<header>'); // Set article header tag
     const $articleHeaderDiv = $('<div>'); // Set div in article header
-    const $headerAvatar = $('<img class="image">').attr('src', data.user.avatars) // Set image tag with avatar
-    const $headerName = $('<span class="name">').html(data.user.name)
-    const $headerHandle = $('<span class="username">').html(data.user.handle)
+    const $headerAvatar = $('<img class="image">').attr('src', data.user.avatars); // Set image tag with avatar
+    const $headerName = $('<span class="name">').html(data.user.name);
+    const $headerHandle = $('<span class="username">').html(data.user.handle);
 
     // Append Header Tags
-    $articleHeaderDiv.append($headerAvatar).append($headerName)
-    $articleHeader.append($articleHeaderDiv).append($headerHandle)
+    $articleHeaderDiv.append($headerAvatar).append($headerName);
+    $articleHeader.append($articleHeaderDiv).append($headerHandle);
 
-    // Tweet Body 
+    // Tweet Body
     const $tweetBody = $('<p>').html(data.content.text);
 
     // Article Footer
     const $articleFooter = $('<footer>');
-    const $timeAgo = $('<span>').html(timeago.format(data.created_at))
-    const $icons = $('<div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>')
+    const $timeAgo = $('<span>').html(timeago.format(data.created_at));
+    const $icons = $('<div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>');
 
     // Append Footer tags
-    $articleFooter.append($timeAgo).append($icons)
+    $articleFooter.append($timeAgo).append($icons);
 
     // Append $output
-    $output.append($articleHeader).append($tweetBody).append($articleFooter)
+    $output.append($articleHeader).append($tweetBody).append($articleFooter);
 
     return $output;
-  }
+  };
 
+  // 2. Function renders multiple tweet articles based on object data
   const renderTweets = function(tweets) {
         
     $('#allTweets').html(''); // Clears default text
-    for (let key in tweets) { // loops through tweets 
+    for (let key in tweets) { // loops through tweets
       $tweet = createTweetElement(tweets[key]); // calls createTweetElement for each tweet
       $('#allTweets').append($tweet); // takes return value and appends it to the tweets container
     }
-  }
+  };
 
-  // Gets JSON from /tweets and renders tweets using the parsed data.
-  $.getJSON('/tweets', function(data){ 
+  // 3. Gets JSON from /tweets and renders tweets using the parsed data.
+  $.getJSON('/tweets', function(data) {
     renderTweets(data);
-  })  
+  });
   
 });
