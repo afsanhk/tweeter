3a51e64f5f9eae239872d52f9c57c58503051d09
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-13 23:28:28 +0000

    Added validation checks within formAjax.js which were checked on the clientside. This was achieved by trimming the previously serialized form content to remove the fieldname= (text=) and using the .length property for comparison with 0 and 140(max) to return alerts.

--- public/scripts/formAjax.js
+++ public/scripts/formAjax.js
@@ -2,11 +2,19 @@ $(document).ready(function () {
   $('.new-tweet').children('form').on('submit', function(event) {
     event.preventDefault();
     const tweetContent = ($(this).serialize()); //$(this).serialize() is whatever is typed into the form!
+    const tweetText = tweetContent.replace('text=',''); // TweetContent without the fieldname
+    const maxLength = 140; 
+
+    // Validation Checks
+    if (tweetText.length > maxLength) { // 1) If the form has more than 140 characters, return an alert and don't post.
+      alert('Please limit your tweet to 140 characters!');
+      return;
+    } else if (tweetText.length === 0) { // 2) If the form is empty, return an alert and don't post.
+      alert('Please make sure to write something!')
+      return;
+    } else {  // 3) Otherwise, post.
+      $.post('/tweets', tweetContent); // This is AJAX shorthand in jQuery
+    }
     
-    // Validation Pseudo
-    /*
-    
-    */
-    $.post('/tweets', tweetContent); // This is AJAX shorthand in jQuery
   });
 });
