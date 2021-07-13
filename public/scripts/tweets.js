c66f959fa69e656c8d1a638a6cdf7589d4048a26
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-13 15:28:27 +0000

    Changed tweets.js so it just shows the oldest tweet prior to review with mentor. Next step will be to create a fixed/sticky nav.

--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -3,13 +3,14 @@ $(document).ready(function() {
 
 
     // This code loads data, but need to do this for all tweets.
-    for(let key in data) {
+    // for(let key in data) { // Use this later when we are trying to add all tweets.
+      let key = 0;
       $('#allTweets').children().children('header').children('div').children('.image').html('<img src="'+data[key].user.avatars+'">');
       $('#allTweets').children().children('header').children('div').children('.name').html(data[key].user.name);
       $('#allTweets').children().children('header').children('.username').html(data[key].user.handle);
       $('#allTweets').children().children('p').html(data[key].content.text);
       $('#allTweets').children().children('footer').children('span').html(timeago.format(data[key].created_at));
-    }
+    // }
   
   })
   
