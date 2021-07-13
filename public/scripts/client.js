63b894748ed592ed5efd97b0ae3b72ba28574c56
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-13 21:29:05 +0000

    Refactored append code to be in one line for each main container within tweet body rather than separate lines.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,17 +5,32 @@
  */
 $(document).ready(function () {
   
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
- }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+
 
   const createTweetElement = function (data) {
     const $output = $('<article class="tweet">'); // Output is a tweet class article
@@ -28,10 +43,8 @@ $(document).ready(function () {
     const $headerHandle = $('<span class="username">').html(data.user.handle)
 
     // Append Header Tags
-    $articleHeaderDiv.append($headerAvatar)
-    $articleHeaderDiv.append($headerName)
-    $articleHeader.append($articleHeaderDiv)
-    $articleHeader.append($headerHandle)
+    $articleHeaderDiv.append($headerAvatar).append($headerName)
+    $articleHeader.append($articleHeaderDiv).append($headerHandle)
 
     // Tweet Body 
     const $tweetBody = $('<p>').html(data.content.text);
@@ -41,19 +54,17 @@ $(document).ready(function () {
     const $timeAgo = $('<span>').html(timeago.format(data.created_at))
     const $icons = $('<div><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>')
 
-    $articleFooter.append($timeAgo)
-    $articleFooter.append($icons)
+    // Append Footer tags
+    $articleFooter.append($timeAgo).append($icons)
 
     // Append $output
-    $output.append($articleHeader)
-    $output.append($tweetBody)
-    $output.append($articleFooter)
+    $output.append($articleHeader).append($tweetBody).append($articleFooter)
 
     return $output;
   }
 
 
-  const $tweet = createTweetElement(tweetData);
+  const $tweet = createTweetElement(data[0]);
   console.log($tweet);
   $('#allTweets').html('');
   $('#allTweets').append($tweet);
