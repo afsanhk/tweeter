44a657981d81fc9653b2f970ab7f8690ad36d411
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-13 16:18:25 +0000

    Change the first <span> element which contained the Avatar image to an <img> tag by default in index.html

--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     // This code loads data, but need to do this for all tweets.
     // for(let key in data) { // Use this later when we are trying to add all tweets.
       let key = 0;
-      $('#allTweets').children().children('header').children('div').children('.image').html('<img src="'+data[key].user.avatars+'">');
+      $('#allTweets').children().children('header').children('div').children('.image').attr("src", data[key].user.avatars);
       $('#allTweets').children().children('header').children('div').children('.name').html(data[key].user.name);
       $('#allTweets').children().children('header').children('.username').html(data[key].user.handle);
       $('#allTweets').children().children('p').html(data[key].content.text);
