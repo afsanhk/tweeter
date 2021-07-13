f8fefb9cae1a429d261ba1e709714f527afde372
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-13 21:44:54 +0000

    A) client.js: 1 - Implemented renderTweets function which renders new tweet elements for each individual tweet data object. 2 - Implement $.getJSON('/tweets,...) to work with active JSON data in renderTweets. B) tweets.css: 1 - Changed styling rules for the tweet container (border removed, flex-direction:column) 2 - Added padding and border styling to tweet articles so that tweets would display individually.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-  
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
-
-
+  // Creates a single tweet article
   const createTweetElement = function (data) {
     const $output = $('<article class="tweet">'); // Output is a tweet class article
     
@@ -63,10 +37,18 @@ $(document).ready(function () {
     return $output;
   }
 
+  const renderTweets = function(tweets) {
+        
+    $('#allTweets').html(''); // Clears default text
+    for (let key in tweets) { // loops through tweets 
+      $tweet = createTweetElement(tweets[key]); // calls createTweetElement for each tweet
+      $('#allTweets').append($tweet); // takes return value and appends it to the tweets container
+    }
+  }
 
-  const $tweet = createTweetElement(data[0]);
-  console.log($tweet);
-  $('#allTweets').html('');
-  $('#allTweets').append($tweet);
+  // Gets JSON from /tweets and renders tweets using the parsed data.
+  $.getJSON('/tweets', function(data){ 
+    renderTweets(data);
+  })  
   
 });
