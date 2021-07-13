1d275fa685c31576d4ab2bfe4ac1e465da476ea4
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-13 23:14:06 +0000

    This branch will be used to add validation for new tweets. 1) Not empty 2) Not longer than 140 characters. It should provide an alert for each scenario, it should not be cleared or submitted.

--- public/scripts/formAjax.js
+++ public/scripts/formAjax.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
   $('.new-tweet').children('form').on('submit', function(event) {
     event.preventDefault();
     const tweetContent = ($(this).serialize()); //$(this).serialize() is whatever is typed into the form!
+    
+    // Validation Pseudo
+    /*
+    
+    */
     $.post('/tweets', tweetContent); // This is AJAX shorthand in jQuery
   });
 });
