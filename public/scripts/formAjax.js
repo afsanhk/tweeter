66f970dbc016c5c438dce1642bf2ae6fd7232c5e
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-14 01:10:00 +0000

    Moved the validation-error clear command outside of the conditional. There was a bug where errors were stacking on top of one another if done consecutively.

--- public/scripts/formAjax.js
+++ public/scripts/formAjax.js
@@ -5,6 +5,9 @@ $(document).ready(function () {
     const tweetText = tweetContent.replace('text=',''); // TweetContent without the fieldname
     const maxLength = 140; 
 
+    // Remove error (if any).
+    $('.new-tweet').children('.validation-error').remove(); 
+
     // Validation Checks
     if (tweetText.length > maxLength) { // 1) If the form has more than 140 characters, return an error message and don't post.
       const $div = $("<div>").addClass('validation-error').text(`Please limit your tweet to ${maxLength} characters!`);
@@ -16,7 +19,7 @@ $(document).ready(function () {
       return;
     } else { // 3) Otherwise, post.
       $.post('/tweets', tweetContent, function () { // AJAX shorthand in jQuery to post.
-        $('.new-tweet').children('.validation-error').remove(); // Remove error.
+        
         $('.new-tweet').children('form')[0].reset(); // Clears the form - source: https://stackoverflow.com/questions/10633605/clear-form-values-after-submission-ajax
         $.getScript('/scripts/client.js'); // AJAX shorthand to get a script - runs tweet loading script again.
       }); 
