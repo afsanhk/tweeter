a64a541f1fdd68f9c54933f6690dc0a463be8bee
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-14 01:05:55 +0000

    Added the jQuery syntax required to add a new div with the validation-error class and corresponding error messages to the respective error cases. Feature is done but currently looking for ways to add a slide on/off animation.

--- public/scripts/formAjax.js
+++ public/scripts/formAjax.js
@@ -6,14 +6,17 @@ $(document).ready(function () {
     const maxLength = 140; 
 
     // Validation Checks
-    if (tweetText.length > maxLength) { // 1) If the form has more than 140 characters, return an alert and don't post.
-      alert('Please limit your tweet to 140 characters!');
+    if (tweetText.length > maxLength) { // 1) If the form has more than 140 characters, return an error message and don't post.
+      const $div = $("<div>").addClass('validation-error').text(`Please limit your tweet to ${maxLength} characters!`);
+      $('.new-tweet').prepend($div); // Prepend so it shows above the form
       return;
-    } else if (tweetText.length === 0) { // 2) If the form is empty, return an alert and don't post.
-      alert('Please make sure to write something!')
+    } else if (tweetText.length === 0) { // 2) If the form is empty, return an error message and don't post.
+      const $div = $("<div>").addClass('validation-error').text('Please make sure to write something!');
+      $('.new-tweet').prepend($div);
       return;
     } else { // 3) Otherwise, post.
       $.post('/tweets', tweetContent, function () { // AJAX shorthand in jQuery to post.
+        $('.new-tweet').children('.validation-error').remove(); // Remove error.
         $('.new-tweet').children('form')[0].reset(); // Clears the form - source: https://stackoverflow.com/questions/10633605/clear-form-values-after-submission-ajax
         $.getScript('/scripts/client.js'); // AJAX shorthand to get a script - runs tweet loading script again.
       }); 
