83819919dddc4048168f60b86a02fa7b2cda8957
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-14 13:41:20 +0000

    Changed the way the error message is displayed. Instead of adding a new div element with validation-error class, we have a pre-existing <div> on index.html with classes validation-error & hidden. While no error persists, the CSS styling keeps the div hidden based on the hidden class. When an error is encountered, the hidden class is removed and content pertaining to the error is added so that the validation-error shows.

--- public/scripts/formAjax.js
+++ public/scripts/formAjax.js
@@ -5,17 +5,17 @@ $(document).ready(function () {
     const tweetText = tweetContent.replace('text=',''); // TweetContent without the fieldname
     const maxLength = 140; 
 
-    // Remove error (if any) -- we might want to do this by adding another class .closed. This will allow us to play with transitions.
-    $('.new-tweet').children('.validation-error').remove(); 
+    // Remove error (if any) - by hiding it. 
+    $('.new-tweet').children('.validation-error').addClass('hidden').text(''); 
 
     // Validation Checks
     if (tweetText.length > maxLength) { // 1) If the form has more than 140 characters, return an error message and don't post.
-      const $div = $("<div>").addClass('validation-error').text(`Please limit your tweet to ${maxLength} characters!`);
+      $('.new-tweet').children('div').removeClass('hidden').text(`Please limit your tweet to ${maxLength} characters!`);
       $('.new-tweet').prepend($div); // Prepend so it shows above the form
       return;
 
     } else if (tweetText.length === 0) { // 2) If the form is empty, return an error message and don't post.
-      const $div = $("<div>").addClass('validation-error').text('Please make sure to write something!');
+      $('.new-tweet').children('div').removeClass('hidden').text('Please make sure to write something!');
       $('.new-tweet').prepend($div);
       return;
 
