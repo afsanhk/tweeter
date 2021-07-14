ce39d53489ba0054137fb913572eff6d0d7849c7
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-14 01:39:27 +0000

    Made some minor changes to formAjax.js as a reminder for what to continue working on tomorrow. Last commit for the workday.

--- public/scripts/formAjax.js
+++ public/scripts/formAjax.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     const tweetText = tweetContent.replace('text=',''); // TweetContent without the fieldname
     const maxLength = 140; 
 
-    // Remove error (if any).
+    // Remove error (if any) -- we might want to do this by adding another class .closed. This will allow us to play with transitions.
     $('.new-tweet').children('.validation-error').remove(); 
 
     // Validation Checks
@@ -13,13 +13,14 @@ $(document).ready(function () {
       const $div = $("<div>").addClass('validation-error').text(`Please limit your tweet to ${maxLength} characters!`);
       $('.new-tweet').prepend($div); // Prepend so it shows above the form
       return;
+
     } else if (tweetText.length === 0) { // 2) If the form is empty, return an error message and don't post.
       const $div = $("<div>").addClass('validation-error').text('Please make sure to write something!');
       $('.new-tweet').prepend($div);
       return;
+
     } else { // 3) Otherwise, post.
       $.post('/tweets', tweetContent, function () { // AJAX shorthand in jQuery to post.
-        
         $('.new-tweet').children('form')[0].reset(); // Clears the form - source: https://stackoverflow.com/questions/10633605/clear-form-values-after-submission-ajax
         $.getScript('/scripts/client.js'); // AJAX shorthand to get a script - runs tweet loading script again.
       }); 
